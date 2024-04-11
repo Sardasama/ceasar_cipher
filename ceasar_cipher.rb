d73b723794c648c36e85e59ffac7724361d1fb82
Sardasama <104152952+Sardasama@users.noreply.github.com>
2024-04-11 13:09:41 +0200

    Update ceasar_cipher.rb

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -2,6 +2,9 @@ def ceasar_cipher(string, shift_factor)
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   alphabet_arr = alphabet.split(//)
   string_arr = string.split(//)
-  crypted_string = string_arr.map { |letter| letter = alphabet_arr[alphabet_arr.index(letter) - shift_factor.to_i]}
+  crypted_string = string_arr.map do |letter|
+    new_index = alphabet_arr.index(letter.downcase).to_i - shift_factor.to_i
+    letter = letter == letter.upcase ? alphabet_arr[new_index].upcase : alphabet_arr[new_index]
+  end
   print crypted_string
 end
